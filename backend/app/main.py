f2c3c8ed90c2426b8f561b77d26f017ca186fa2e
Laxmikant Vishwakarma <laxmikant051096@gmail.com>
2026-02-23 20:52:10 +0530

    All other changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,16 @@ from app.routes.prediction_route import router as prediction_router
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow all origins for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
     return {"message": "Medical Assistant API is running"}
